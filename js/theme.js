655c832788edd7d7be893fce56c7910b1b136255
mickytroxxy <mickylyricalkartel@gmail.com>
2023-01-31 23:51:23 +0200

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -62,7 +62,7 @@
             var mapOptions = {
               zoom: 15,
               scrollwheel: false,
-              center: new google.maps.LatLng(40.7127837, -74.00594130000002)
+              center: new google.maps.LatLng(-26.215260, 28.036560)
             };
             var map = new google.maps.Map(document.getElementById('map'),
                 mapOptions);
